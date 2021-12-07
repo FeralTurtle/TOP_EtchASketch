42203d541dd4649aa0b8100b42f9ce521cbe2af6
Jeremy T <jeremytupas@gmail.com>
2021-12-06 20:19:58 -0600

    Added CSS variables

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
-let gridChoice = 16;
-let gridSize = gridChoice*gridChoice;
+//let gridChoice = 16;
 const gridContainer = document.querySelector(".grid-container");
 let counter = 0;
+//CSS variables
+let root = document.querySelector(':root');
+let rootStyles = getComputedStyle(root);
+let gridSize = rootStyles.getPropertyValue('--grid-size');
+let drawColor = rootStyles.getPropertyValue('--draw-color');
+//root.style.setProperty('--draw-color', '#ffffff');
+//root.style.setProperty('--grid-size', 32);
 
+let tileCount = gridSize*gridSize;
 //Fill grid with divs
-while (counter < gridSize) {
+while (counter < tileCount) {
     counter++;
     const gridDiv = document.createElement("div");
     gridContainer.appendChild(gridDiv);
 }
 const gridItems = gridContainer.querySelectorAll("div.grid-container div");
-gridItems.forEach(item => item.style.backgroundColor="#2d5986");
-
+gridItems.forEach(item => item.style.backgroundColor='var(--draw-color)');
